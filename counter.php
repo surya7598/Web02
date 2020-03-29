0a7c6ed2d23215ad012e3afe6474f49c6ba2c63a
surya7598 <surya.lesmana7598@gmail.com>
2020-03-29 15:06:27 +0700

    publish file Web02

---
+++ counter.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<title>Contoh Counter</title>
+</head>
+<body>
+<?php
+	$nama_file="counter.dat";
+	if (file_exists($nama_file))
+	{
+		$berkas = fopen($nama_file,"r");
+		$pencacah = (integer)trim(fgets($berkas, 255));
+		$pencacah++;
+		fclose($berkas);
+	}
+	else
+		$pencacah = 1;
+		// simpan pencacah
+		$berkas = fopen($nama_file,"w");
+		fputs($berkas, $pencacah);
+		fclose($berkas);
+		// tulis ke halaman web
+		Print("Anda pengunjung ke-$pencacah <br>\n"); 
+?>
+</body>
+</html> 
---
+++ latihan.php
@@ -0,0 +1,19 @@
+
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+switch ($color) {
+	case 1 "red":
+		echo "Hello";
+    break;
+	case 2 "green":
+		echo "Welcome";
+    break;
+}
+?>
+
+</body>
+</html>
